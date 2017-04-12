e213aacf96949c2e5e01ce33abd02372b93b5be2
Guillaume Massé <masgui@gmail.com>
2017-04-12 19:15:07 -0400

    manually box ptr to avoid Scala-Native bugs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import stdio._
 
 import ncurses._, ncursesh._
 import posix._, posixh._
-import ifaddrs._, ifaddrsh._
+import Ifaddrs._, IfaddrsH._
 
 /*
 
@@ -23,53 +23,71 @@ import ifaddrs._, ifaddrsh._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    
+    print("\033c")
     findInterface.foreach(println)
 
+    // var timer = 0L
+    // var redraw = false
+    // val tv = stackalloc[timeval]
 
-    var timer = 0L
-    var redraw = false
-    val tv = stackalloc[timeval]
+    // while(true) {
+    //   gettimeofday(tv, null)
+    //   if (timer < tv.tv_sec) {
+    //     timer = tv.tv_sec
+    //     redraw = true;
+    //   }
 
-    while(true) {
-      gettimeofday(tv, null)
-      if (timer < tv.tv_sec) {
-        timer = tv.tv_sec
-        redraw = true;
-      }
-
-      if(redraw) {
-        println("draw")
-        redraw = false
-      }
-    }
+    //   if(redraw) {
+    //     println("draw")
+    //     redraw = false
+    //   }
+    // }
   }
 
   def findInterface: Option[String] = {
-    val ifaddrs = stackalloc[Ptr[ifaddrs]]
-    var ifa = stackalloc[ifaddrs]
+    val ifaddrs = stackalloc[Ptr[Ifaddrs]]
+    // var ifa = stackalloc[Ifaddrs]
 
     if (getifaddrs(ifaddrs) != -1) {
-      ifa = !ifaddrs
 
-      var found = false
-      while(ifa != null && !found) {
-        import SiocgifFlags._
+      // ifa = !ifaddrs
 
-        if((ifa.flags & Up) &&
-           (ifa.flags & Running) &&
-          !(ifa.flags & Loopback)
-        ) {
-          found = true
-        }
-        else {
-          ifa = ifa.next
-        }
-      }
+      // var found = false
+      // while(ifa != null && !found) {
+      //   import SiocgifFlags._
 
-      val ret =
-        if(found) Some(fromCString(ifa.name))
-        else None
+      //   if((ifa.flags & Up) &&
+      //      (ifa.flags & Running) &&
+      //     !(ifa.flags & Loopback)
+      //   ) {
+      //     found = true
+      //   }
+      //   else {
+      //     ifa = ifa.next
+      //   }
+      // }
+
+      // val ret =
+      //   if(found) Some(fromCString(ifa.name))
+      //   else None
+
+      import SiocgifFlags._
+
+      val ret = 
+        (!ifaddrs).iterator
+          .find(ifa => {
+            println(fromCString(ifa.name))
+
+            println("  "          + ifa.flags.value)
+            println("  Up      :" + (ifa.flags & Up))
+            println("  Running :" + (ifa.flags & Running))
+            println("  Loopback:" + (ifa.flags & Loopback))
+
+             (ifa.flags & Up) &&
+             (ifa.flags & Running) &&
+            !(ifa.flags & Loopback)
+          })
+          .map(ifa => fromCString(ifa.name))
 
       freeifaddrs(!ifaddrs)
 
--- src/main/scala/ifaddrs.scala
+++ src/main/scala/ifaddrs.scala
@@ -1,14 +1,14 @@
 import scalanative.native._
 
 @extern
-object ifaddrs {
-  import ifaddrsh._
+object Ifaddrs {
+  import IfaddrsH._
 
-  def getifaddrs(ifap: Ptr[Ptr[ifaddrs]]): CInt = extern
-  def freeifaddrs(ifa: Ptr[ifaddrs]): Unit = extern
+  def getifaddrs(ifap: Ptr[Ptr[Ifaddrs]]): CInt = extern
+  def freeifaddrs(ifa: Ptr[Ifaddrs]): Unit = extern
 }
 
-object ifaddrsh {
+object IfaddrsH {
   class SiocgifFlags(val value: CInt) extends AnyVal {
     def &(other: SiocgifFlags): Boolean = 
       (value & other.value) == other.value
@@ -20,22 +20,35 @@ object ifaddrsh {
     // ...
   }
 
-  type ifaddrs = CStruct7[
+  type Ifaddrs = CStruct7[
     Ptr[CInt],     // [1] ifa_next
     Ptr[CChar],    // [2] ifa_name
     CInt,          // [3] ifa_flags
-    Ptr[sockaddr], // [4] ifa_addr
-    Ptr[sockaddr], // [5] ifa_netmask
-    Ptr[sockaddr], // [6] ifa_ifu
+    Ptr[SockAddr], // [4] ifa_addr
+    Ptr[SockAddr], // [5] ifa_netmask
+    Ptr[SockAddr], // [6] ifa_ifu
     Ptr[Byte]      // [7] ifa_data
   ]
 
-  implicit class ifaddrsOps(ptr: Ptr[ifaddrs]) {
-    def next: Ptr[ifaddrs] = (!(ptr._1)).cast[Ptr[ifaddrs]]
+  implicit class IfaddrsOps(val ptr: Ptr[Ifaddrs]) extends AnyVal {
+    def next: Ptr[Ifaddrs] = (!(ptr._1)).cast[Ptr[Ifaddrs]]
     def name: Ptr[CChar] = !(ptr._2)
     def flags: SiocgifFlags = new SiocgifFlags(!(ptr._3))
+
+    def iterator: Iterator[IfaddrsOps] = new Iterator[IfaddrsOps]{
+      private var current = new IfaddrsOps(ptr)
+
+      def hasNext: Boolean = {
+        current.ptr.next != null
+      }
+
+      def next(): IfaddrsOps = {
+        current = new IfaddrsOps(current.next)
+        current
+      }
+    }
   }
-  
+
   class SaFamily(val value: CInt)
   object SaFamily {
     final val AF_PACKET = new SaFamily(17)
@@ -45,12 +58,12 @@ object ifaddrsh {
   final val InterfaceNameSize = 14
   type _14 = Nat.Digit[Nat._1, Nat._4]
 
-  type sockaddr = CStruct2[
+  type SockAddr = CStruct2[
     CInt,              // [1] sa_family
     CArray[CChar, _14] // [2] sa_data
   ]
 
-  class sockaddrOps(val ptr: Ptr[sockaddr]) {
+  class SockAddrOps(val ptr: Ptr[SockAddr]) {
     def family: SaFamily = new SaFamily(!(ptr._1))
     def data: CArray[CChar, _14] = !(ptr._2)
   }
